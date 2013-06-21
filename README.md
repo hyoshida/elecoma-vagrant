ff99c37e89903355d308f0d1350c31490aedb225
YOSHIDA Hiroki <hyoshida@appirits.com>
2013-06-21 14:17:05 +0900

    setup.shを追加

--- README.md
+++ README.md
@@ -5,10 +5,6 @@ Please enter the following line in the command line:
 
     git clone git://github.com/hyoshida/elecoma-vagrant.git
     cd elecoma-vagrant
-    bundle install --path vendor/bundle
-    bundle exec berks install --path cookbooks
-    bundle exec vagrant up
-    bundle exec vagrant ssh-config --host=elecoma-vagrant > elecoma-vagrant-ssh.conf
-    bundle exec knife solo bootstrap elecoma-vagrant -F elecoma-vagrant-ssh.conf
+    ./setup.sh
 
 Then, please go to ``localhost:8080`` in your browser.
---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+print() {
+    printf "\033[1;32m$*\033[0;39m\n"
+}
+
+error() {
+    printf "\033[1;31mAn error occurred!\033[0;39m"
+    exit 1
+}
+
+# Gemをインストール
+print "Install gems..."
+bundle install --path vender/bundle || error
+
+# クックブックをインストール
+print "Install cookbooks..."
+bundle exec berks install --path cookbooks || error
+
+# VM起動
+print "Startup vagrant box..."
+bundle exec vagrant up || error
+
+# VMのSSH設定を出力
+bundle exec vagrant ssh-config --host=elecoma-vagrant > elecoma-vagrant-ssh.conf || error
+
+# Chefのインストールとクックブック適用 (bootstrap = prepare + cook)
+print "Setup elecoma-vagrant..."
+bundle exec knife solo bootstrap elecoma-vagrant -F elecoma-vagrant-ssh.conf || error
+
+print "Successed!"
